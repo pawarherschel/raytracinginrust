a9666971fcddec2b3f4a2e9c01f5bea403828a0d
pawarherschel <pawarherschel@gmail.com>
2023-11-23 14:48:27 +0530

    using vec3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{stderr, Write};
+
+use raytracing::Color;
+
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for j in 0..IMAGE_HEIGHT {
+        eprintln!("Scanlines remaining: {}", IMAGE_HEIGHT - j - 1);
+        stderr().flush().unwrap();
+        for i in 0..IMAGE_WIDTH {
+            let pixel_color = Color::new(
+                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
+                (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
+                0.25,
+            );
+
+            println!("{}", pixel_color.fmt_color());
+        }
+    }
+
+    eprintln!("\x07Done");
 }
